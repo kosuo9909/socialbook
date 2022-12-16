abd9b6b7829435e613e3180e40093588df16bbaa
Kostadin <stanevbsn@gmail.com>
2022-12-16 11:26:05 +0200

    image upload validation

--- post/forms.py
+++ post/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from post.models import PostMaker, LikePhoto, CommentPhoto
 
@@ -7,6 +8,16 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = PostMaker
         fields = ['image', 'caption']
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image', None)
+
+        if image:
+            if image.size > 5*1024*1024:
+                raise ValidationError('The image file is too large ( > 5 MB )')
+            return image
+        else:
+            raise ValidationError('Could not read the uploaded image')
 
 
 class LikeForm(forms.ModelForm):
